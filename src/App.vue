c920057883351f7dec12d3d11f6f32a63364ee33
JunHaoShih <victor5517@gmail.com>
2023-03-07 13:35:31 +0800

    chore: add dynamic banner

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,20 @@
     <router-link to="/todo">Todo list</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <div class="font-bold text-white bg-neutral-800">Hello World</div>
+  <div class="font-bold text-white bg-neutral-800">{{ currentRouteName }}</div>
   <router-view/>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class App extends Vue {
+  get currentRouteName(): string {
+    return this.$route.name?.toString() || '';
+  }
+}
+</script>
 
 <style>
 @import "assets/main.css";
