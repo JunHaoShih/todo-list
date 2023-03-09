29c24ac8d6eb7f65b85564161a83dee3c966c954
JunHaoShih <victor5517@gmail.com>
2023-03-09 12:08:00 +0800

    fix: change MyItem to tr

--- src/modules/todo-list/components/MyItem.vue
+++ src/modules/todo-list/components/MyItem.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="flex group">
-    <label v-bind:for="task.id.toString()" class="block mb-2 flex-auto text-sm font-medium
-      text-gray-900
-      dark:text-gray-300">
-      <input v-bind:id="task.id.toString()" type="checkbox" value="" v-model="task.isDone"
-        class="w-4 h-4 text-blue-600 mx-6
-        bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
-        dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        {{ task.taskName }}
-    </label>
-    <button type="button" class="text-white
+  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700
+    hover:bg-gray-50 dark:hover:bg-gray-600">
+    <td class="w-4 p-4">
+      <div class="flex items-center">
+        <label v-bind:for="task.id.toString()" class="">
+          <input v-bind:id="task.id.toString()" type="checkbox" value="" v-model="task.isDone"
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded
+              focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800
+              dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        </label>
+      </div>
+    </td>
+    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+      {{ task.taskName }}
+    </th>
+    <td class="flex items-center px-6 py-4 space-x-3">
+      <button type="button" class="text-white
       bg-red-700 hover:bg-red-800 focus:outline-none
       focus:ring-4  focus:ring-red-300 font-medium rounded-full
-      text-sm px-5 py-2.5 text-center mr-8 mb-2
-      dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900
-      opacity-0 group-hover:opacity-100"
+      text-sm px-5 py-2.5 text-center mb-2
+      dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
       @click="onRemoveClicked">Remove</button>
-  </div>
+    </td>
+  </tr>
 </template>
 
 <script lang="ts">
